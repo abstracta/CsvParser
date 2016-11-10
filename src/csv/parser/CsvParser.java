a9eb97b47366ccd6ece2beacda9c5d44f7cbb93e
ndarriulat <nicolas.darriulat@abstracta.com.uy>
2016-11-10 17:41:06 -0300

    New exception when the delimiter does not exist.

--- src/csv/parser/CsvParser.java
+++ src/csv/parser/CsvParser.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 /**
  *
  * @author Nicolas Darriulat
- * github username: ndarriulat
+ * @github ndarriulat
  * 
  */
 public class CsvParser {
@@ -26,21 +26,30 @@ public class CsvParser {
      * @param delimiter Specifies the String with which the CSV will be split.
      * @return Returns a Object[][], which contains every row in the CSV file.
      * @throws FileNotFoundException If the CSV file is not found in the specified path, a FileNotFoundException will be thrown.
+     * @throws csv.parser.DelimiterNotFoundException If one line of the file does not contain the given delimiter, a DelimiterNotFoundException will be thrown.
      */
-    public static Object[][] generateObjectMatrixFromCsv(String path,boolean ignoreFirstRow,boolean ignoreFirstColumn,String delimiter) throws FileNotFoundException{
+    public static Object[][] generateObjectMatrixFromCsv(String path,boolean ignoreFirstRow,boolean ignoreFirstColumn,String delimiter) throws FileNotFoundException, DelimiterNotFoundException{
         Scanner scanner = new Scanner(new File(path));
+        int lineNumber=1;
         if (!scanner.hasNext()) {
             return null;
         }
         scanner.useDelimiter(delimiter);
         if (ignoreFirstRow) {
-            scanner.nextLine();            
+            String firstLine=scanner.nextLine();          
+            verifyLineContainsDelimiter(firstLine,delimiter,lineNumber);
         }        
         
         ArrayList<Object[]> list=new ArrayList<>();
         // Iterate the csv and add data to the list
         while(scanner.hasNext()){
-            String[] parameters=scanner.nextLine().split(delimiter);
+            String line=scanner.nextLine();
+            lineNumber++;
+            
+            // Verify that the delimiter exists in the line.
+            verifyLineContainsDelimiter(line,delimiter,lineNumber);
+            String[] parameters=line.split(delimiter);
+            
             if (ignoreFirstColumn) {
                 String[] newParameters=new String[parameters.length-1];
                 System.arraycopy(parameters, 1, newParameters,0,newParameters.length);
@@ -57,6 +66,12 @@ public class CsvParser {
         }
         Object[][] matrix=convertListToMatrix(list);
         return matrix;
+    }
+    
+    private static void verifyLineContainsDelimiter(String line,String delimiter,int lineNumber) throws DelimiterNotFoundException{
+        if (!line.contains(delimiter)) {
+                throw new DelimiterNotFoundException("The delimiter '"+delimiter+"' was not found at line "+lineNumber+" of the CSV file.");
+        }
     }
     
     private static Object[][] convertListToMatrix(ArrayList<Object[]> list){
---
+++ src/csv/parser/DelimiterNotFoundException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csv.parser;
+
+/**
+ *
+ * @author Nicolas Darriulat
+ * @github ndarriulat
+ */
+public class DelimiterNotFoundException extends Exception{
+
+    public DelimiterNotFoundException(String message) {
+        super(message);
+    }
+    
+}
